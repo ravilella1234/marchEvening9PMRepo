34b18efc1275e33764431e0ce5a79952b692768a
maha <maha@gmail.com>
2020-03-19 21:56:13 +0530

    Browser profiles & options Classes

---
+++ MarchEvening9PMBatch/src/com/launchings/TC_003.java
@@ -0,0 +1,16 @@
+package com.launchings;
+
+public class TC_003 extends BaseTest
+{
+
+	public static void main(String[] args) throws Exception 
+	{
+		init();
+		
+		openBrowser("chromebrowser");
+		
+		navigateUrl("iciciurl");
+
+	}
+
+}
